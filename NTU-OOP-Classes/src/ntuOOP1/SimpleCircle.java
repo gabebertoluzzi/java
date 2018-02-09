3c1b2001af37de91021a93abc1f48cbbee6b795a
GabeYA <gabe.bertoluzzi@gmail.com>
2018-02-09 11:30:25 -0600

    Starting InvoiceItem

---
+++ NTU-OOP-Classes/src/ntuOOP1/SimpleCircle.java
@@ -0,0 +1,41 @@
+package ntuOOP1;
+
+public class SimpleCircle {
+	// private instance variable
+	private double radius;
+	
+	// First constructor, default radius
+	public SimpleCircle() {
+		this.radius = 1.0;
+	}
+	
+	// Second constructor, given radius variable
+	public SimpleCircle(double radius) {
+		this.radius = radius;
+	}
+	
+	// Getter for radius variable
+	public double getRadius() {
+		return this.radius;
+	}
+	
+	// Setter for radius variable
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+	
+	// Getter for Area of circle (PI r ^ 2)
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
+	
+	// Getter for Circle's Circumference (2PIr)
+	public double getCircumference() {
+		return 2*Math.PI*radius;
+	}
+	
+	// SimpleCircle toString (Circle[radius=?])
+	public String toString() {
+		return "Circle[radius=" + this.radius + "]";
+	}
+}
