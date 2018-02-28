8cbf7bf4767237a622218d15d24f4509de054f66
Gabe Bertoluzzi <gbertoluzzi@mednetstudy.com>
2018-02-27 20:37:31 -0600

    Started GUI OOP work.
    Stuck on factorial logic.

---
+++ GUI/src/OneAWT/AWTAccumulator.java
@@ -0,0 +1,52 @@
+package OneAWT;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AWTAccumulator extends Frame implements ActionListener {
+	private Label lblInput;
+	private Label lblOutput;
+	private TextField tfInput;
+	private TextField tfOutput;
+	private int sum = 0;
+	
+	// Constructor to setup the UI components and event handlers
+	public AWTAccumulator() {
+		setLayout(new FlowLayout());		// "super" Frame sets to FlowLayout
+		
+		lblInput = new Label("Enter an integer");
+		add(lblInput);
+		
+		tfInput = new TextField(10);
+		add(tfInput);
+		
+		tfInput.addActionListener(this);
+		
+		lblOutput = new Label("Factorial is ");
+		add(lblOutput);
+		
+		tfOutput = new TextField(10);
+		tfOutput.setEditable(false);
+		add(tfOutput);
+		
+		setTitle("AWT Accumulator");
+		setSize(500, 120);
+		setVisible(true);
+	}
+	
+	// the entry main() method
+	public static void main(String[] args) {
+		// Invoke constructor by allocating anonymous Accumulator instance
+		new AWTAccumulator();
+	}
+	
+	// ActionEvent handler - called back when enter key was hit on TextField.
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		int numberIn = Integer.parseInt(tfInput.getText());
+		sum += numberIn;
+		for (int i = 0; i < )
+		tfInput.setText("");
+		tfOutput.setText("Accumulated sum is: " + sum);
+	}
+	
+}
---
+++ GUI/src/OneAWT/AWTCounter.java
@@ -0,0 +1,72 @@
+package OneAWT;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class AWTCounter extends Frame implements ActionListener {
+	// Declare component Label
+	private Label lblCount;
+	// Declare component TextField
+	private TextField tfCount;
+	// Declare component Label
+	private Label lblFactorial;
+	// Declare component TextField
+	private TextField tfFactorial;
+	// Declare component Button
+	private Button btnCount;
+	// Counter's value
+	private int count = 0;
+	// factorial value
+	private int factorial = count;
+	
+	// Constructor to setup UI components and event handlers
+	public AWTCounter() {
+		setLayout(new FlowLayout());
+		// 'super' Frame sets layout to FlowLayout, which arranges
+		//   components from left-to-right, then top-to-bottom
+		
+		lblCount = new Label("n");		// Construct component Label
+		add(lblCount);						// "super" Frame adds Label
+		
+		// Counter textfield
+		tfCount = new TextField(count + "", 5);
+		tfCount.setEditable(false);
+		add(tfCount);
+		
+		// Factorial Label
+		lblFactorial = new Label("factorial(n)");
+		add(lblFactorial);
+		
+		// Factorial Textfield
+		tfFactorial = new TextField(factorial + "", 5);
+		tfFactorial.setEditable(false);
+		add(tfFactorial);
+		
+		
+		btnCount = new Button("Next");
+		add(btnCount);
+		btnCount.addActionListener(this);
+		
+		setSize(250, 100);
+		setTitle("AWT Counter - n!");
+		setVisible(true);			// show "super" Frame
+	}
+	
+	// ActionEvent handler = Called back when the button is clicked
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		++count;
+		tfCount.setText(count + "");
+		
+		factorial = count * count;
+		tfFactorial.setText(factorial + "");
+		
+	}
+	
+	// the entry main() method
+	public static void main(String[] args) {
+		// Invoke constructor by allocating anonymous instance
+		new AWTCounter();
+	}
+
+}
