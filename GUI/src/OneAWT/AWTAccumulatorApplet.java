ea86993aa3915bf5c84b8256abd0493d0f670062
Gabe Bertoluzzi <gbertoluzzi@mednetstudy.com>
2018-03-02 00:06:42 -0600

    Applet complete/deprecated

--- GUI/src/OneAWT/AWTAccumulatorApplet.java
+++ GUI/src/OneAWT/AWTAccumulatorApplet.java
@@ -1,5 +1,30 @@
 package OneAWT;
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
 
-public class AWTAccumulatorApplet {
-
+// an applet extends java.applet.Applet
+public class AWTAccumulatorApplet extends Applet implements ActionListener {
+	private TextField tfInput;
+	private int sum = 0;
+	
+	// init() runs when the applet is loaded.Setup the UI components and event
+	public void init() {
+		add(new Label("Enter an integer"));		// Anonymous label
+		
+		tfInput = new TextField(10);
+		add(tfInput);
+		tfInput.addActionListener(this);
+			// Hitting enter key on tfInput invokes actionPerformed
+	}
+	
+	// ActionEvent handler - Called back when enter key was hit on TextField.
+	public void actionPerformed(ActionEvent evt) {
+		int numberIn = Integer.parseInt(evt.getActionCommand());
+			// getActionCommand returns the String entered
+		sum += numberIn;
+		tfInput.setText("");
+		showStatus("The accumulated sum is: " + sum);
+	}
 }
