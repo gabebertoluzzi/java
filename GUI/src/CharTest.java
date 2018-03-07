9b5b8b72b85629a32643e2e38886befff33006cd
Gabe Bertoluzzi <gbertoluzzi@mednetstudy.com>
2018-03-07 12:41:17 -0600

    just some test work

---
+++ GUI/src/CharTest.java
@@ -0,0 +1,14 @@
+
+
+
+public class CharTest {
+
+	public static void main(String args[]) {
+	      String s = "Strings are immutable";
+	      char result = s.charAt(8);
+	      System.out.println(result);
+	      
+	      int val = s.charAt(8);
+	      System.out.println(val);
+	}
+}
